a75e0f6566dabafa61663504644bed79ced7f655
Román Núñez <roma.nunez@duocuc.cl>
2026-09-25 18:36:04 -0300

    Entrega final

--- bff/src/main/java/ms_pedidos_360/bff/controller/GraphUserController.java
+++ bff/src/main/java/ms_pedidos_360/bff/controller/GraphUserController.java
@@ -1,15 +1,11 @@
 package ms_pedidos_360.bff.controller;
 
-
 import lombok.RequiredArgsConstructor;
 
 import ms_pedidos_360.bff.dto.graph.OperatorResponse;
 import ms_pedidos_360.bff.service.GraphUserService;
-
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
 
 
 @RestController
@@ -17,20 +13,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class GraphUserController {
 
-
-
     private final GraphUserService graphUserService;
-
-
-
     @GetMapping("/operators")
     public List<OperatorResponse> obtenerOperadores(){
-
-
         return graphUserService.obtenerOperadores();
-
-
     }
-
-
 }
